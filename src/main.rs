368ca930eb8b7e9d804179141664d6bf0ead1689
Felix Vadcard <vadcard.felix@gmail.com>
2024-02-23 18:03:13 +0100

    adding files to fill up one by one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod errors;
+mod get;
+mod init;
+mod install;
+mod memory;
+mod project_dirs;
+mod registry;
+mod set;
+mod types;
+mod uninstall;
+mod update;
+
 fn main() {
     println!("Hello, world!");
 }

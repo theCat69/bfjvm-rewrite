b441a0a6a148cff3233f4c6b674ac0a12893b4c6
Felix Vadcard <vadcard.felix@gmail.com>
2024-02-24 10:17:06 +0100

    starting to implements java types

--- src/types/java.rs
+++ src/types/java.rs
@@ -1,1 +1,14 @@
-pub enum JavaDistibution {}
+pub enum JavaDistribution {
+    OpenJdk,
+}
+
+// maybe is should have multiple struct depending on what i want or i can
+// use underlying lazyly initialize attribute on this struct
+// like checking if the candidate is the curent one or if it is installed
+pub struct JavaCandidate {
+    version: u16,
+    exact_version: u16,
+    download_url: Option<String>,
+    latest: bool,
+    distribution: JavaDistribution,
+}
